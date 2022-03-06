164f6c04d114c2774f8e767e8f7058ae3a3be6f8
Gbenga Adeyi <adeyigbenga005@gmail.com>
2022-03-06 23:08:39 +0100

    Add model defintion for store app

--- apps/store/models.py
+++ apps/store/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    price = models.DateField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product-image/image/images')
+
+    def __str__(self) -> str:
+        return self.image.filename
