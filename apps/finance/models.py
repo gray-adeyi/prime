1693804d2a4441c516c4fec1bb8e2916d2c4c7bd
Gbenga Adeyi <adeyigbenga005@gmail.com>
2022-03-06 23:08:03 +0100

    Add model definition for finance app

--- apps/finance/models.py
+++ apps/finance/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Block(models.Model):
+    date = models.DateField()
+
+
+class Transaction(models.Model):
+    STATUS_OPTIONS = (
+        (0, 'pending'),
+        (1, 'approved'),
+        (2, 'declined'),
+    )
+
+    block = models.ForeignKey(Block, on_delete=models.CASCADE)
+    customer = models.ForeignKey()
+    amount = models.DecimalField()
+    status = models.CharField(max_length=2, choices=STATUS_OPTIONS)
+    timestamp = models.DateTimeField(auto_now_add=True)
