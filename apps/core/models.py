f7b774b23b437cbb055e38c59f7bc09c1edf6e43
Gbenga Adeyi <adeyigbenga005@gmail.com>
2022-03-06 23:05:11 +0100

    Add core app model definitions

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Site(models.Model):
+    """
+    Model holds website data.
+    """
+    name = models.CharField(max_length=50)
+    favion = models.ImageField(upload_to='site/favicon/images')
+    appleicon = models.ImageField(upload_to='site/appleicon/images')
+    logo = models.ImageField(upload_to='site/logo/images')
+
+    def __str__(self) -> str:
+        return self.name
