c6743b933283cb333f64631832661cea22fa376c
Gbenga Adeyi <adeyigbenga005@gmail.com>
2022-03-06 23:07:13 +0100

    Add model definition for editor app

--- apps/editor/models.py
+++ apps/editor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Editor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(
+        upload_to='editor/profile_picture/images')
+
+    def __str__(self) -> str:
+        return self.user.firstname
