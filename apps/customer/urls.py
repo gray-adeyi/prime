d4f522ec74789f519a1fec6d25fa7572b93c683b
Gbenga Adeyi <adeyigbenga005@gmail.com>
2022-03-13 13:48:37 +0100

    add login,logout and dashboard urls

--- apps/customer/urls.py
+++ apps/customer/urls.py
@@ -1,5 +1,11 @@
+from unicodedata import name
 from django.urls import path
+from . import views
 
 app_name = 'customer'
 
-url_patterns = []
+urlpatterns = [
+    path('login/', views.login_customer, name='login'),
+    path('logout/', views.logout_customer, name='logout'),
+    path('dashboard/', views.Dashboard.as_view(), name='dashboard'),
+]
