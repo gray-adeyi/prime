8809ca80c6d25d374aa696183d52468df9c0720e
Gbenga Adeyi <adeyigbenga005@gmail.com>
2022-03-13 13:43:05 +0100

    add login view to editor app

--- apps/editor/views.py
+++ apps/editor/views.py
@@ -1,3 +1,15 @@
+from django.views.generic import TemplateView
+from django.http import HttpResponseNotAllowed
 from django.shortcuts import render
 
-# Create your views here.
+
+def login_editor(request):
+    template_name = "editor/login.html"
+    if request.method == "GET":
+        return render(request, template_name,)
+    elif request.method == "POST":
+        pass
+    else:
+        return HttpResponseNotAllowed(
+            ["POST", "GET"]
+        )
